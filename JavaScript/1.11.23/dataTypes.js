bad3ade1de19c6abc223d52b28af2fd555f23448
NoamMoskowitz <noammz101@gmail.com>
2023-11-01 21:13:19 +0200

    1.11.23 Class

---
+++ JavaScript/1.11.23/dataTypes.js
@@ -0,0 +1,37 @@
+function abc(){
+    console.log("String With Your Name");
+console.log("\"Noam\" it\'s \\ \n noammz101@gmail.com");
+console.log("4 Integers");
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log("4 Floats");
+console.log(1.2);
+console.log(1.3);
+console.log(1.4);
+console.log(1.5);
+console.log("4 Long Numbers");
+console.log(9_999_999_999);
+console.log(10_000_000_000);
+console.log(12_162_345_678);
+console.log(29_345_890_723);
+console.log("Bolean Values");
+console.log(true);
+console.log(false);
+console.log("Void Values")
+console.log(undefined);
+console.log(null);
+console.log("Object");
+console.log({
+    name: "Noam",
+    Age: 23 ,
+    Email: "noammz101@gmail.com",
+    address:{
+        city:"Bet Shemesh",
+        Street:"Harav Kuk",
+        number:2
+    }
+});
+}
+abc();
