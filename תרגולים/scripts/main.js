6d629a8be32b8501a2767da3f3c3427a1e2ab742
Noam Moskowitz <noammz101@gmail.com>
2023-12-07 12:56:17 +0200

    updated user management project

--- תרגולים/scripts/main.js
+++ תרגולים/scripts/main.js
@@ -6,30 +6,71 @@ for (user of users) {
     let row = usersTable.insertRow();
     for (key in user) {
         let cell = row.insertCell();
-        let text = document.createTextNode(user[key]);
-        cell.appendChild(text);
+        cell.textContent = user[key];
     }
-    let cell = row.insertCell();
-    let button1 = document.createElement('button');
-    let button2 = document.createElement('button');
-    let button3 = document.createElement('button');
-    let btnText1 = document.createTextNode('התחברות');
-    let btnText2 = document.createTextNode('התנתקות');
-    let btnText3 = document.createTextNode('מחיקה');
-    button1.classList.add('btn');
-    button2.classList.add('btn');
-    button3.classList.add('btn');
-    button1.classList.add('m-1');
-    button2.classList.add('m-1');
-    button3.classList.add('m-1');
-    button1.appendChild(btnText1);
-    button1.classList.add('btn-alert')
-    cell.appendChild(button1);
-    button2.appendChild(btnText2);
-    button2.classList.add('btn-secondary')
-    cell.appendChild(button2);
-    button3.appendChild(btnText3);
-    button3.classList.add('btn-primary')
-    cell.appendChild(button3);
 
+    let cell = row.insertCell();
+    let disconnect = document.createElement('button');
+    let remove = document.createElement('button');
+    let edit = document.createElement('button');
+    let buttonDiv = document.createElement('div');
+    buttonDiv.append(disconnect, remove, edit);
+
+    for (children of buttonDiv.children) {
+        children.classList.add('btn');
+        children.classList.add('m-1');
+    }
+
+    remove.textContent = 'מחיקה';
+    edit.textContent = 'עריכה';
+    disconnect.textContent = 'התנתקות';
+
+    remove.classList.add('btn-alert')
+    disconnect.classList.add('btn-secondary')
+    edit.classList.add('btn-primary')
+
+    cell.appendChild(buttonDiv);
 }
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(event);
+
+    const user = {
+        fname: event.target.form['fname'].value,
+        lname: event.target.form['lname'].value,
+        email: event.target.form['email'].value,
+        password: event.target.form['password'].value,
+        isLoggedIn: false
+    }
+    let row = usersTable.insertRow();
+
+    for (key in user) {
+        let cell = row.insertCell();
+        cell.textContent = user[key];
+    }
+
+    let cell = row.insertCell();
+    let disconnect = document.createElement('button');
+    let remove = document.createElement('button');
+    let edit = document.createElement('button');
+    let buttonDiv = document.createElement('div');
+    buttonDiv.append(disconnect, remove, edit);
+
+    for (children of buttonDiv.children) {
+        children.classList.add('btn');
+        children.classList.add('m-1');
+    }
+
+    remove.textContent = 'מחיקה';
+    edit.textContent = 'עריכה';
+    disconnect.textContent = 'התנתקות';
+
+    remove.classList.add('btn-alert')
+    disconnect.classList.add('btn-secondary')
+    edit.classList.add('btn-primary')
+
+    cell.appendChild(buttonDiv);
+    event.target.form.reset()
+
+});
