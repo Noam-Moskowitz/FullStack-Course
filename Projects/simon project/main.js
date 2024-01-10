a33edabf2bcfeccf792efb1433ad4ac5eaada87b
Noam Moskowitz <noammz101@gmail.com>
2024-01-10 14:58:55 +0200

    work on simon prject

--- Projects/simon project/main.js
+++ Projects/simon project/main.js
@@ -1,59 +1,54 @@
 let arrRound = [];
-let arrPlayer=[];
-let gameCounter;
+let gameCounter; // Game counter
 let userCounter;
-    let i=0;
 
-
+// Function (A)
+// Resets the game and ready to play
+// This does NOT start the game, you need to press a button to start the game
 function initGame() {
-    console.clear();
-    gameCounter = userCounter = 1;
-    arrPlayer=[];
+    // console.clear();
+    gameCounter = userCounter = 0;
     arrRound = [];
-    console.log(`Initiating Game...`);
+    console.log('Game initiated...');
 }
 
+// Function (B)
 function playRound() {
-    presentColor()
-   
-    
-    
+    // 1. Create step and add as a last element to the array
+    const newStep = createStep();
+    arrRound.push(newStep);
+    console.log(arrRound);
+
+    // Deteremine that computer's turn is fininshed, and waiti for user input
+    // ??
+
 }
 
+function userTurn(inpt) {
+    let i = 0;
+    console.log(inpt);
+    // Compare input with
+    if (inpt == arrRound[i]) {
+        console.log('You guessed correctly');
+        gameCounter++;
+        i++;
+        playRound();
+    } else {
+        console.log('You lose, game over');
+        // initGame();
+    }
+}
+
+// Function (C)
+// 1. Creates a random number from 1 to 4
+// 2. Returns the result and/or the color (צבע שמייצג את המספר)
 function createStep() {
-    const arrColors=[`red`,`green`,`blue`,`yellow`]
-    return arrColors[Math.floor(Math.random()*4)]
-}
-function presentColor(){
-    for(color of arrRound){
-        console.log(color);
-        setTimeout(()=>,500)
-    document.getElementById(color).style.opacity=`1`;
-    setTimeout(()=>{
-        document.getElementById(color).style.opacity=`.5`;
-    },600)
-    }
+    const arrColors = [`red`, `green`, `blue`, `yellow`]
+    return arrColors[Math.floor(Math.random() * 4)]
 }
 
-function eventHandler() {
-    initGame();
-    arrRound.push(createStep())
-    playRound()
-}
-document.addEventListener(`click`,(event)=>{
-    if(event.target.id!=`btn`){
-    arrPlayer.push(event.target.id);
-    console.log(arrPlayer);
-     for(move of arrPlayer){
-        if(move == arrRound[i]){
-             arrRound.push(createStep())
-             console.log(arrRound);
-             playRound()
-
-        } else{
-            console.log(`loser`);
-        }
-    }
-    }
+initGame();
+document.getElementById('btnStart').addEventListener('click', playRound);
+document.querySelector(`.gameButtons`).addEventListener(`click`, (event) => {
+    userTurn(event.target.id);
 })
-document.getElementById(`btn`).addEventListener(`click`, eventHandler);
