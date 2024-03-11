60de32146f514a550bb27a9d503251df4a2126b9
Noam Moskowitz <noammz101@gmail.com>
2024-03-11 15:00:05 +0200

    10.3.24 homework

--- React/10.3.24/my-app/src/components/auth/Login.jsx
+++ React/10.3.24/my-app/src/components/auth/Login.jsx
@@ -1,23 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
 
 export default function Login() {
+
+  const [user, setUser] = useState({
+    email: ``,
+    password: ``
+  })
+
+  const [status, setStatus] = useState(``);
+
+
+  const handleChange = (e) => {
+    const key = e.target.name;
+    const value = e.target.value;
+
+    setUser({
+      ...user,
+      [key]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const hardcodedUser = {
+      email: `noammz101@gmail.com`,
+      password: `qwerty123`
+    };
+
+    try {
+      if (hardcodedUser.email === user.email &&
+        hardcodedUser.password === user.password) {
+        setStatus(`Login Succesful!`)
+      } else {
+        throw new Error(`Login Failed, Credentials Not Valid!`)
+      }
+    } catch (error) {
+      setStatus(error.message)
+    }
+
+  }
+
+
   return (
     <div className='form-styling'>
-        <h1>Login</h1>
-        <form className='loginForm' name='logIn'>
-            <div className="input-container">
-                <label htmlFor="logIn">Email:</label>
-                <input type="email" form='logIn' />
-            </div>
-            <div className="input-container">
-                <label htmlFor="logIn">Password:</label>
-                <input type="password" form='logIn' />
-            </div>
+      <h1>Login</h1>
+      <form className='loginForm' onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id='email'
+            name='email'
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name='password'
+            id='password'
+            onChange={handleChange}
+          />
+        </div>
 
-            <input htmlFor='logIn' type="submit" value="Submit" />
+        <input
+          type="submit"
+          value="Submit"
+        />
 
-        </form>
+      </form>
+
+      <div>{status}</div>
     </div>
   )
 }
