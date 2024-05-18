4f9e4d2de419401a34aea9e361d120cf05fb8570
NoamMoskowitz <noammz101@gmail.com>
2024-05-18 21:40:14 +0300

    class 16.5.24 hw

--- React/15.5.24/reducer-prectice-app/src/components/ProductGallery.jsx
+++ React/15.5.24/reducer-prectice-app/src/components/ProductGallery.jsx
@@ -53,7 +53,7 @@ const ProductGallery = () => {
             <div onMouseLeave={()=> setHoveredItem(null)} className='w-4/6 grid grid-cols-3 grid-rows-3 p-36 gap-8  '>
                 {products.map(product=>(
                     <div
-                        className=' flex flex-col justify-start items-center p-5 border-2 rounded  shadow-md hover:shadow-2xl ' 
+                        className=' flex flex-col justify-start items-center p-5 border-2 rounded  shadow-md hover:shadow-2xl relative ' 
                         key={product.id}
                         onMouseEnter={()=>setHoveredItem(product)}
                     >
@@ -70,14 +70,13 @@ const ProductGallery = () => {
                             {hoveredItem===product && (
                                 <div className=''>
                                     <Link to={`/update_product/${product.id}`}>
-                                        <button className='text-sm bg-gray-200 p-1 rounded hover:bg-gray-100 '>Edit</button>
+                                        <button className='text-md bg-blue-400 p-1 rounded-2xl uppercase px-2 hover:bg-blue-200  absolute top-0 left-0 m-2'>Edit</button>
                                     </Link>
-                                    <button 
-                                        onClick={()=>removeItem(product.id)}
-                                        className='text-sm bg-gray-200 p-1 rounded hover:bg-gray-100 ml-2'
-                                    >
-                                        Delete
-                                    </button>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" onClick={()=>removeItem(product.id)}
+                                        className='text-sm bg-red-400 p-1 rounded hover:bg-red-200 hover:cursor-pointer ml-2 absolute right-0 top-0 w-8 m-1.5'>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+
                                 </div>
                             )}
                         </div>
